073db1ea2f0972e949dcc72f2945f039fb431f42
Abir-ux <abir573912@gmail.com>
2020-06-20 15:17:48 +0600

    onek kiso sikhse

---
+++ doble-vs-triple.js
@@ -0,0 +1,20 @@
+const first = 2;
+const second = "2";
+if (first == second){
+    console.log("true");
+}
+else{
+    console.log(false);
+}
+
+
+
+
+const first1 = 2;
+const second1 = "2";
+if (first1 === second1){
+    console.log("true");
+}
+else{
+    console.log(false);
+}
